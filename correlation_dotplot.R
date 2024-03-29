55d3a25ea12d6f83be275a118b0ce4e3269471dc
yueli8 <40526984+yueli8@users.noreply.github.com>
2024-03-29 21:01:55 +0800

    Delete correlation_dotplot.R

--- correlation_dotplot.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-setwd("~/gse205506/finalize")
-a<-read.table("ANPEP_CA2", head=TRUE, sep="\t")
-ggplot(a,aes(x=features.plot,y=id))+geom_point()
-ggplot(a,aes(x=features.plot,y=id))+geom_point(aes(size=`correlation`,
-                 color=`chisq.test`))  
